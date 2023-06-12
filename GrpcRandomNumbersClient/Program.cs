589f7abdab306a930b6e8b1fbe2bfd9c91dc8d3f
Garkavyi Artem <artemoniks@yandex.ru>
2023-06-12 09:54:39 +0300

    Initial commit

---
+++ GrpcRandomNumbersClient/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Text.Json;
+using Google.Protobuf;
+using Google.Protobuf.WellKnownTypes;
+using Grpc.Net.Client;
+using GrpcRandomNumbersClient;
+
+//For local run
+//using var channel = GrpcChannel.ForAddress("https://localhost:7280");
+
+using var channel = GrpcChannel.ForAddress("http://host.docker.internal:8080");
+var client = new Numbers.NumbersClient(channel);
+
+Console.WriteLine("Start...");
+
+for (var j = 0; j < 10; j++)
+{
+    for (var i = 0; i < 100; i++)
+    {
+        var reply = await client.GetNumberAsync(new Empty());
+        Console.WriteLine("Random number: " + GetNumber(reply));
+        await Task.Delay(100);
+    }
+    
+    await Task.Delay(1000);
+}
+
+Console.WriteLine("End...");
+
+
+long GetNumber(NumberReply numberReply)
+{
+    var json = JsonFormatter.Default.Format(numberReply.Data);
+    var document = JsonDocument.Parse(json);
+
+    long.TryParse(document.RootElement.GetProperty("number").GetString(), out var result);
+    return result;
+}
---
+++ ReadME.md
@@ -0,0 +1,17 @@
+### Build docker image
+
+*docker client must be run*
+
+Run at the root path of the project:
+
+        docker build -t grpcrandomnumbersclient:1.0 ./
+
+### Run container
+
+Then run the container:
+
+        docker run grpcrandomnumbersclient:1.0 
+
+You can run containers as much as you want. 
+
+A client does 100 requests with a delay of 100 ms and then repeats it 10 times with a delay of 1 s.
